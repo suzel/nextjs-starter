6074c203a59cf4fec8efea8748ce7c79f763a8c6
Sukru Uzel <sukru.uzel@gmail.com>
2025-02-10 16:56:57 +0300

    update config

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Header />
-      <body>{children}</body>
-      <Footer />
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
